86f1ef502408279dba5f90e86ee9cc9d9b65c6e9
pedrogpteixeira <pgpteixeira9@gmail.com>
2026-05-27 05:01:44 +0100

    feat: add home layout with sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () =>
-      import('./features/auth/login/login').then((m) => m.Login),
+    loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
   },
   {
     path: 'signup',
+    loadComponent: () => import('./features/auth/signup/signup').then((m) => m.Signup),
+  },
+  {
+    path: 'home',
     loadComponent: () =>
-      import('./features/auth/signup/signup').then((m) => m.Signup),
+      import('./layout/home-layout/home-layout').then((m) => m.HomeLayout),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/home/dashboard/dashboard').then((m) => m.Dashboard),
+      },
+    ],
   },
   {
     path: '',
---
+++ src/app/layout/home-layout/home-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Sidebar } from '../../shared/components/sidebar/sidebar';
+
+@Component({
+  selector: 'app-home-layout',
+  imports: [RouterOutlet, Sidebar],
+  templateUrl: './home-layout.html',
+  styleUrl: './home-layout.scss',
+})
+export class HomeLayout {}
